4ee43eb8a0679a1805eb0f215c4267198253cc6d
stacccy <stacccy@H1-Ap-2016003.local>
2017-11-17 15:53:08 +0800

    add react-router / redux stores / notFoundPage / action constants

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { connect } from 'react-redux';
+
+class NotFound extends React.Component {
+    render() {  
+        return (
+            <div>
+                <h1>Page Not Found</h1>
+                <p>The page you are looking for doesn't exist.</p>
+            </div>
+        )
+    }
+}
+
+export default connect(
+    (state, ownProps) => ({})
+)(NotFound);
